4a96b0e801a6de5acd4c98e341f26d1b62263fd3
KAY1020 <53653084+KAY1020@users.noreply.github.com>
2021-02-09 15:49:35 +0800

    All movie listed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Movie from './movie';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  //not fast all the time. So we need to tell JavaScript that we might need to wait for some time until getMovies function finishes
+  getMovies = async () => {
+    //this function is asynchronous so we need to wait for it. 
+   
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    //telling to wait for axios to finish using await
+
+    this.setState({movies, isLoading: false});
+    //equal to {movies:movies}
+  }
+
+  componentDidMount(){
+    this.getMovies();
+  }
+
+  render() {
+    const {isLoading, movies} = this.state;
+    //ES6 
+    return (
+      <section className = "container">
+        {isLoading ? (
+          <div className = "loader">
+            <span className = "loader__text">
+              Loading...
+            </span>
+          </div>
+        ) : (
+          <div className = "movies">
+            {movies.map(movie => {
+            //console.log(movie);
+              return <Movie 
+                    key= {movie.id} 
+                    id ={movie.id} 
+                    year={movie.year} 
+                    title= {movie.title} 
+                    summary= {movie.summary} 
+                    poster ={movie.medium_cover_image}
+                    genre = {movie.genres}/>
+          })}
+          </div>
+        )} 
+      </section>
+    );
+  }
 }
 
 export default App;
